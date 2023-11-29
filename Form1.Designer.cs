9264e0bab143847fc77751bdcd9f948040d7d7b7
basmala <basmalaelshabrawy4@gmail.com>
2023-11-29 10:32:40 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace WindowsFormsApp13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textrows = new System.Windows.Forms.TextBox();
+            this.textcolumns = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelMatrix = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // textrows
+            // 
+            this.textrows.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textrows.Location = new System.Drawing.Point(81, 19);
+            this.textrows.Name = "textrows";
+            this.textrows.Size = new System.Drawing.Size(57, 23);
+            this.textrows.TabIndex = 0;
+            this.textrows.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textcolumns
+            // 
+            this.textcolumns.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textcolumns.Location = new System.Drawing.Point(81, 61);
+            this.textcolumns.Margin = new System.Windows.Forms.Padding(5);
+            this.textcolumns.Name = "textcolumns";
+            this.textcolumns.Size = new System.Drawing.Size(57, 23);
+            this.textcolumns.TabIndex = 1;
+            this.textcolumns.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(187, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Matrix : ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panelMatrix
+            // 
+            this.panelMatrix.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panelMatrix.Location = new System.Drawing.Point(268, 3);
+            this.panelMatrix.Name = "panelMatrix";
+            this.panelMatrix.Size = new System.Drawing.Size(314, 145);
+            this.panelMatrix.TabIndex = 3;
+            this.panelMatrix.Paint += new System.Windows.Forms.PaintEventHandler(this.panelMatrix_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Rows";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Column";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Blue;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(187, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 37);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Solve";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(119, 163);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(211, 104);
+            this.panel1.TabIndex = 10;
+            // 
+            // panel2
+            // 
+            this.panel2.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel2.ForeColor = System.Drawing.Color.Blue;
+            this.panel2.Location = new System.Drawing.Point(57, 284);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(413, 49);
+            this.panel2.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 404);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelMatrix);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textcolumns);
+            this.Controls.Add(this.textrows);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textrows;
+        private System.Windows.Forms.TextBox textcolumns;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelMatrix;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp13
+{
+    public partial class Form1 : Form
+    {
+        private int row;
+        private int col;
+        private double[,] matrix;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            row = int.Parse(textrows.Text);
+            col = int.Parse(textcolumns.Text);
+            matrix = new double[row, col];
+            CreateMatrixInputs();
+        }
+        private void CreateMatrixInputs()
+        {
+            panelMatrix.Controls.Clear();
+            int x = 20;
+            int y = 20;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    TextBox txtBox = new TextBox();
+                    Label label = new Label();
+                    label.TextAlign = ContentAlignment.MiddleCenter;
+                    label.Text = $"X{j}";
+                    if (j == col - 1)
+                        label.Text = $"B";
+                    label.Location = new System.Drawing.Point(x, 0);
+                    label.Size = new System.Drawing.Size(40, 20);
+                    txtBox.Name = "txtMatrix_" + i + "_" + j;
+                    txtBox.Location = new System.Drawing.Point(x, y);
+                    txtBox.Size = new System.Drawing.Size(40, 20);
+                    panelMatrix.Controls.Add(txtBox);
+                    panelMatrix.Controls.Add(label);
+                    x += 50;
+                }
+                x = 20; y += 30;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ReadMatrixValues();
+            void ReadMatrixValues()
+            {
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < col; j++)
+                    {
+                        TextBox txtBox = (TextBox)panelMatrix.Controls["txtMatrix_" + i + "_" + j];
+                        matrix[i, j] = double.Parse(txtBox.Text);
+                    }
+                }
+            }
+            for (int i1 = 0; i1 < row; i1++)
+            {
+
+                int maxRow = 0;
+                for (int j = i1 + 1; j < row; j++)
+                {
+                    if (matrix[i1, i1] == 0)
+                    {
+                        maxRow = j;
+                        break;
+                    }
+                }
+
+                double[] temp = new double[row + 1];
+                for (int k = 0; k < row + 1; k++)
+                {
+                    temp[k] = matrix[i1, k];
+                    matrix[i1, k] = matrix[maxRow, k];
+                    matrix[maxRow, k] = temp[k];
+                }
+
+                for (int i = 0; i < row; i++)
+                {
+                    
+
+                    for (int j = i + 1; j < row; j++)
+                    {
+                        double d = matrix[i, i] * matrix[j, j] - matrix[i, j] * matrix[j, i];
+                        if (d == 0)
+                        {
+                            MessageBox.Show("The system has many solution.");
+                            return;
+                        }
+                        double factor = -matrix[j, i] / matrix[i, i];
+                        for (int k = i; k < col; k++)
+                        {
+                            matrix[j, k] += factor * matrix[i, k];
+                        }
+                    }
+                }
+
+                for (int i = 0; i < row; i++)
+                {
+                    double x1 = matrix[i, i];
+                    for (int j = 0; j < col; j++)
+                    {
+                        matrix[i, j] = matrix[i, j] / x1;
+                    }
+                }
+                int x = 20; int y = 20;
+
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < col; j++)
+                    {
+                        TextBox txtBox = new TextBox();
+                        txtBox.Name = "txtMatrix_" + i + "_" + j;
+                        txtBox.Location = new System.Drawing.Point(x, y);
+                        txtBox.Size = new System.Drawing.Size(40, 20);
+                        Label label = new Label();
+                        label.TextAlign = ContentAlignment.MiddleCenter;
+                        label.Text = $"X{j}";
+                        if (j == col - 1)
+                            label.Text = $"B";
+                        label.Location = new System.Drawing.Point(x, 0);
+                        label.Size = new System.Drawing.Size(40, 20);
+                        panel1.Controls.Add(txtBox);
+                        panel1.Controls.Add(label);
+                        txtBox.Text = $"{matrix[i, j]}";
+                        x += 50;
+                    }
+                    x = 20; y += 30;
+                }
+
+                double[] solutionn = new double[row];
+                for (int i = row - 1; i >= 0; i--)
+                {
+                    solutionn[i] = matrix[i, row] / matrix[i, i];
+                    for (int k = i - 1; k >= 0; k--)
+                        matrix[k, row] -= matrix[k, i] * solutionn[i];
+                }
+
+                int c = 20;
+                for (int i = 0; i < row; i++)
+                {
+                    TextBox textBox = new TextBox();
+                    textBox.Location = new System.Drawing.Point(c, 20);
+                    textBox.Size = new System.Drawing.Size(70, 40);
+                    panel2.Controls.Add(textBox);
+                    c += 70;
+                }
+
+                for (int i = 0; i < row; i++)
+                {
+                    TextBox textBox = (TextBox)panel2.Controls[i];
+                    textBox.Text = $" X{i + 1} = {solutionn[i]}";
+                }
+            }
+        }
+        private void panelMatrix_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+   
+}
+       
+    
+
